052509ea7d306b9e3841d6718fc16955a6d8fba2
vigneshwaran <vignesh510510@gmail.com>
2023-10-12 16:34:19 +0530

    Updated

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,47 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from '../assets/image/logo.jpg';
-
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping, faUser } from '@fortawesome/free-solid-svg-icons';
+import logo from '../assets/image/logo_2.gif';
 
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const cartItemCount = 1; 
 
-function Header() {
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div>
-     <nav className="navbar navbar-expand-lg navbar-dark fixed-top portfolio-navbar" style={{ color: '#182C61', paddingTop: '5px', paddingBottom: '5px' }}>
-  <div className="container">
-    <Link to="/" className="navbar-brand d-flex align-items-center justify-content-center justify-content-lg-start">
-      <img src={logo} className="img-fluid" style={{ maxHeight: '60px', mixBlendMode: 'multiply', backgroundColor: 'rgba(255, 255, 255, 0.5)' }} alt="" />
-    </Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" style={{ padding: '15px' }}>
-      <span className="navbar-toggler-icon" style={{ backgroundColor: '#182C61', borderRadius: '5px' }}></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav ms-auto align-items-center justify-content-center justify-content-lg-start">
-
-        <li className="nav-item">
-          <Link to="/#Home" className="nav-link" id="nav" smooth>
-            Home
-          </Link>
-        </li>
-
-        <li className="nav-item">
-          <Link to="/Projects" className="nav-link" smooth>
-            Projects
-          </Link>
-        </li>
+    <nav className="navbar navbar-expand-lg navbar-light fixed-top" style={{ backgroundColor: 'white',marginTop:'0px',paddingTop:'0px',fontFamily:'Arial' }}>
+      <div className="container">
         
-        <li className="nav-item">
-          <Link to="/login" className="nav-link" style={{ backgroundColor: '#45aaf2', fontWeight: 500, color: 'white', padding: '8px 16px', borderRadius: '5px', letterSpacing: '1px' }}>
-            Login
-          </Link>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-    </div>
-  )
+        {/* Logo */}
+        <Link to="/" className="navbar-brand">
+          <img src={logo} alt="Logo" style={{ maxHeight: '60px',marginTop:'0px', marginRight:'100px' }} />
+        </Link>
+       
+        {/* Burger Icon for Mobile View */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          onClick={toggleNavbar}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Navbar Links */}
+        <div className={`collapse navbar-collapse  ${isOpen ? 'show' : ''}`} id="navbarNav">
+       
+          <ul className="navbar-nav mx-auto text-center">
+            <li className="nav-item">
+              <Link to="/" className="nav-link"  style={{ color: 'black', fontWeight:600,marginRight:'20px' }}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link"  style={{ color: 'black', fontWeight:600,marginRight:'20px' }}>
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contact" className="nav-link"  style={{ color: 'black', fontWeight:600,marginRight:'20px' }}>
+                Contact
+              </Link>
+            </li>
+            <li className="nav-item mx-auto text-center">
+              <Link to="/cart" className="nav-link" style={{ color: 'black', fontWeight: 600,marginRight:'30px'}}>
+                Cart | {cartItemCount > 0 && <span className="badge bg-success ms-1 small align-middle">{cartItemCount}</span>}
+                {' Items'} <FontAwesomeIcon icon={faCartShopping} style={{ color: '#f39c12', fontSize:'20px'}} className='align-middle' />
+              </Link>
+            </li>
+          </ul>
+         
+          <ul className="navbar-nav">
+            <li className="nav-item mx-auto text-center">
+              <Link to="/login" className="nav-link"  style={{ color: 'black', fontWeight:600, marginRight:'5px'}}>
+                Login  <FontAwesomeIcon icon={faUser} style={{ color: '#00a8ff'}} />
+              </Link>
+            </li>
+          </ul>
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <form className="d-flex ">
+                <input className="form-control form-control border-info" type="search" placeholder="Search for Products ..." aria-label="Search" style={{ marginRight: '5px' }} />
+                <button className="btn btn-outline-info" type="submit">
+                  Search
+                </button>
+              </form>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Header
+export default Navbar;
